c56140d616b9bf282b6388a51fda03179f9aa0a1
Nunzio Martucci <nunziomart98@gmail.com>
2022-02-21 12:24:37 +0100

    terminate rotte e controller

--- todo/app/Http/Controllers/TodoController.php
+++ todo/app/Http/Controllers/TodoController.php
@@ -7,79 +7,28 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
-        //
+        $todo = Todo::all();
+        return view('home', compact('todo'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+       $data = $request->validate([
+           'contenuto' => 'required'
+       ]);
+
+       Todo::create($data);
+       return back();
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function show(todo $todo)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(todo $todo)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, todo $todo)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\todo  $todo
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy(todo $todo)
     {
-        //
+        $todo->delete();
+        return back();
     }
 }
