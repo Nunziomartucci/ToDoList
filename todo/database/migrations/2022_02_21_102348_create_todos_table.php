aedc13692cf3e3273b12b850fa666249062da0bb
Nunzio Martucci <nunziomart98@gmail.com>
2022-02-21 11:45:10 +0100

    creazione della migrazione su database

--- todo/database/migrations/2022_02_21_102348_create_todos_table.php
+++ todo/database/migrations/2022_02_21_102348_create_todos_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Symfony\Component\Mime\Encoder\ContentEncoderInterface;
 
 return new class extends Migration
 {
@@ -15,6 +16,7 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('contenuto');
             $table->timestamps();
         });
     }
